e6626d88d326194720b5912e6bc736e3ded8ed30
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-05-27 21:23:47 +0800

    Mel-Frequency Cepstral Coefficients, MFCC

--- predict.py
+++ predict.py
@@ -5,7 +5,9 @@ import os
 from tqdm import tqdm
 import torchaudio
 
-model = torch.load('model.pt')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = torch.load('model.pt').to(device)
 
 test_data_dir = 'public_test/public_test/'
 
@@ -14,15 +16,15 @@ X = torch.FloatTensor([])
 for f in tqdm(files[:]):
     # 聲音訊號
     waveform, sample_rate = torchaudio.load(test_data_dir+f)
-    # 聲音頻譜圖
-    specgram = torchaudio.transforms.Spectrogram()(waveform)
+    # 聲音頻譜圖 梅尔频率倒谱系数（Mel-Frequency Cepstral Coefficients, MFCC）
+    specgram = torchaudio.transforms.MelSpectrogram()(waveform)
     specgram = torch.unsqueeze(specgram, 0)
     # 加入X
     X = torch.cat([X,specgram],dim=0)
 
 # 推論
-X = torch.FloatTensor(X)
-y_hat = model(X).detach().numpy()
+X = torch.FloatTensor(X).to(device)
+y_hat = model(X).detach().cpu().numpy()
 print(y_hat.shape)
 
 submit = pd.DataFrame(columns=['Filename','Barking','Howling','Crying','COSmoke','GlassBreaking','Other'])
